17526e8016ef256a0e479755149c4e3b0f51987b
Giannis Savvidis <giannis.savvidis@slashdata.co>
2024-05-31 23:42:37 +0300

    switch to yarn, create env vars for firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,8 @@ export default defineNuxtConfig({
   runtimeConfig: {
     public: {
       apiUrl: "",
+      firebaseApiKey: "",
+      firebaseAuthDomain: "",
     },
   },
   devtools: { enabled: true },
--- plugins/vueFire.client.ts
+++ plugins/vueFire.client.ts
@@ -2,14 +2,13 @@ import { VueFire, VueFireAuth } from "vuefire";
 import { initializeApp } from "firebase/app";
 
 export default defineNuxtPlugin((app) => {
+  const {
+    public: { firebaseApiKey, firebaseAuthDomain },
+  } = useRuntimeConfig();
+
   const firebaseApp = initializeApp({
-    apiKey: "AIzaSyDAzkBkt58hK28Haik2SCbda2yEN74mvhk",
-    authDomain: "budget-calculator-dev.firebaseapp.com",
-    projectId: "budget-calculator-dev",
-    storageBucket: "budget-calculator-dev.appspot.com",
-    messagingSenderId: "177554562557",
-    appId: "1:177554562557:web:d181e042f7573a52b10d94",
-    measurementId: "G-3J7HPNWFGC",
+    apiKey: firebaseApiKey,
+    authDomain: firebaseAuthDomain,
   });
 
   app.vueApp.use(VueFire, {
